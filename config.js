18f6206d690f4fa807723683a39fbac8d5eacab5
Jannik Lorenz <git@janniklorenz.de>
2015-06-06 22:53:57 +0200

    create basic files, add some demo configs

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+
+	/**
+		path to the presentation folder
+	*/
+	presentationPath: "presentation1/",
+
+
+	/**
+		main content array, which contains an array for each stripes
+	*/
+	slides: [
+
+		/**
+			stripe (array with pages)
+		*/
+		[
+			// relativ path to pageFolder
+			"page1",
+			"page2",
+		],
+		[
+			"page3"
+		]
+
+	]
+
+
+}
---
+++ presentation1/page2/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+	/**
+		npm name of the plugin which should be used
+		you have to set it up as a dependencie in the package json or install it manually
+	*/
+	npm-name: "pluginName"
+
+}
